8fb451fe2692092b73185358d89348fab02cb50d
Elian Marsault <elian.marsault@wedia.group>
2025-10-08 17:24:05 +0200

    Add simplified GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/skin-tone-variator/',
   server: {
     port: 3000,
     host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 });
